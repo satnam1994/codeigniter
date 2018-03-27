47b152a609333123732b6a8327d1a37f307f4097
satnam1994 <singh.sat111@gmail.com>
2018-03-27 19:42:38 +0530

    add code for post data

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -17,4 +17,21 @@ class Api extends CI_Controller {
 			print_r(json_encode(array('RESPONSE' => $this->input->get())));
 		}
 	}
+	public function post_data()
+	{
+		if (empty($this->input->server('PHP_AUTH_USER')))
+	    {
+			header('HTTP/1.0 401 Unauthorized');
+			header('HTTP/1.1 401 Unauthorized');
+			header('WWW-Authenticate: Basic realm="My Realm"');
+			echo 'You must login to use this service'; // User sees this if hit cancel
+			die();
+		}else{
+
+			$username = $this->input->server('PHP_AUTH_USER');
+			$password	 = $this->input->server('PHP_AUTH_PW');
+			
+			print_r(json_encode(array('RESPONSE' => $this->input->post())));
+		}
+	}
 }
